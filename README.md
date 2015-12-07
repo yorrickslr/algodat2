4cb2611808bc521a52f1ad9c7862f4d5ef0ff443
yorrickslr <yoyo.sieler@gmail.com>
2015-12-07 23:30:49 +0100

    add template function and double comparison

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ created by Yorrick Sieler-Morzuch `114560`
 
 > please note:
 > the C++-code must be compiled with C++11 standard activated [ -std=c++11 ]
+
+Usage:
+You can either type in a string as one argument or a list of doubles, separated by spaces.
--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <string>
 #include <algorithm>
 
-void quickSort(std::vector<char>::iterator begin, std::vector<char>::iterator end) {
+template<typename Iter>
+void quickSort(Iter begin, Iter end) {
   if(begin==end) {
     return;
   }
-  std::vector<char>::iterator pivot, min, max;
+  Iter pivot, min, max;
   pivot = begin + std::distance(begin, end) / 2;
   std::iter_swap(pivot, end-1);
   pivot = end-1;
@@ -31,16 +32,34 @@ void quickSort(std::vector<char>::iterator begin, std::vector<char>::iterator en
 }
 
 int main(int argc, char *argv[]) {
-  if(argc!=2) {
-    std::cout << "Usage: " << argv[0] << " <characters to be sorted>" << std::endl;
+  if(argc<2) {
+    std::cout << "Usage: " << argv[0] << " \"characters to be sorted\"" << std::endl;
+    std::cout << "Usage: " << argv[0] << " <n1> <n2> ... <n> " << std::endl;
     return 0;
+  } else if(argc==2) {
+    std::string input = argv[1];
+    std::vector<char> toSort{input.begin(), input.end()};
+    quickSort(toSort.begin(), toSort.end());
+    std::cout << "Final: ";
+    for(char c : toSort) {
+      std::cout << c;
+    }
+    std::cout << std::endl;
+  } else {
+    std::vector<double> toSort;
+    try {
+      for(int i=1; i<argc; i++) {
+        toSort.push_back(std::stod(argv[i]));
+      }
+    } catch(...) {
+      std::cout << "Error while parsing list" << std::endl;
+      return 1;
+    }
+    quickSort(toSort.begin(), toSort.end());
+    std::cout << "Final: ";
+    for(double d : toSort) {
+      std::cout << d << " ";
+    }
+    std::cout << std::endl;
   }
-  std::string input = argv[1];
-  std::vector<char> toSort{input.begin(), input.end()};
-  quickSort(toSort.begin(), toSort.end());
-  std::cout << "Final: ";
-  for(char c : toSort) {
-    std::cout << c;
-  }
-  std::cout << std::endl;
 }
